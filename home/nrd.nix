ffdc541b2200f6094c399f3fa7ffa71c0dc3d2c5
Timothy DeHerrera <tim@nrdxp.dev>
2022-04-14 00:50:58 -0600

    use dunst for notifications

--- home/nrd.nix
+++ home/nrd.nix
@@ -6,5 +6,8 @@
     imports = [ ./alacritty ./git ./direnv ];
 
     programs.git = lib.importTOML ./gitconfig.toml;
+    services.dunst.enable = true;
+    services.dunst.configFile = ./dunstrc;
+    services.dunst.waylandDisplay = "wayland-0";
   };
 }
