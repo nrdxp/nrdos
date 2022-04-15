0a9f2faf09467c2e8e56b303692fbb77d69186b6
Timothy DeHerrera <tim@nrdxp.dev>
2022-04-15 17:06:07 -0600

    add wofi config files

--- profiles/graphical/wayland/default.nix
+++ profiles/graphical/wayland/default.nix
@@ -9,16 +9,25 @@
     swaybg
     (waybar.override { pulseSupport = config.hardware.pulseaudio.enable; })
     wl-clipboard
-    wofi
+    (wofi.overrideAttrs (_: {
+      preFixup = ''
+        gappsWrapperArgs+=(
+          --set XDG_CONFIG_HOME "/etc/xdg"
+        )
+      '';
+    }))
     wofi-emoji
   ];
+
 
   programs.tmux.extraConfig = lib.mkBefore ''
     set -g @override_copy_command 'wl-copy'
   '';
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
   environment.etc."xdg/waybar".source = ./waybar;
+  environment.etc."xdg/wofi".source = ./wofi;
 
   xdg.portal = {
     enable = true;
