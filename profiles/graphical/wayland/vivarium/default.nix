eeec82ce53284ece7c7c139e1630a6c97195178b
Timothy DeHerrera <tim@nrdxp.dev>
2022-04-17 17:38:47 -0600

    configure vivarium in NixOS module

--- profiles/graphical/wayland/vivarium/default.nix
+++ profiles/graphical/wayland/vivarium/default.nix
@@ -1,14 +1,37 @@
 { inputs, pkgs, ... }:
+let
+  configFile =
+    pkgs.writeText "config.toml" ''
+      ${builtins.readFile ./config.toml}
+
+      ### BACKGROUND ###
+      # The background options are displayed using `swaybg`. Make sure you have this installed
+      # if you want to use them.
+      [background]
+      colour = "#bbbbbb"
+      image = "${./bg.jpg}"
+      mode = "fill"
+    '';
+
+  vivarium = pkgs.vivarium.overrideAttrs
+    (self: {
+      nativeBuildInputs = self.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+      postInstall = self.postInstall + ''
+        wrapProgram "$out/bin/vivarium" \
+         --add-flags '--config ${configFile}'
+      '';
+    });
+in
 {
   imports = [ ../. ];
 
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
     vivarium
   ];
 
   nixpkgs.overlays = [ inputs.vivarium.overlay ];
 
-  services.xserver.displayManager.sessionPackages = [ pkgs.vivarium ];
+  services.xserver.displayManager.sessionPackages = [ vivarium ];
 
   services.blueman.enable = true;
 }
