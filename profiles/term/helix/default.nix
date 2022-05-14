f05e802e1aff1b65c52492e49afad183079ba41b
Timothy DeHerrera <tim@nrdxp.dev>
2022-05-14 16:00:08 -0600

    helix: add configuration

--- profiles/term/helix/default.nix
+++ profiles/term/helix/default.nix
@@ -1,6 +1,8 @@
 {
   inputs,
   pkgs,
+  lib,
+  config,
   ...
 }: {
   environment.systemPackages = with pkgs; [
@@ -11,5 +13,30 @@
     cmake-language-server
     taplo-lsp
     rnix-lsp
+  ];
+  home-manager.sharedModules = lib.mkIf (config ? home-manager) [
+    {
+      programs.helix.enable = true;
+      programs.helix.package = inputs.helix.defaultPackage.${pkgs.system};
+      programs.helix.settings = {
+        theme = "snazzy";
+        keys.normal = {
+          C-s = ":w";
+          C-q = ":q";
+          C-w = "rotate_view";
+          y = ["yank" "yank_joined_to_clipboard"];
+        };
+        keys.insert.j.j = "normal_mode";
+      };
+      programs.helix.languages = [
+        {
+          name = "toml";
+          language-server = {
+            command = "taplo-lsp";
+            args = ["run"];
+          };
+        }
+      ];
+    }
   ];
 }
