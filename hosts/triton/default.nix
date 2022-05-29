66a0dfe4d3c95141783b70fc58a03dff75314f06
Timothy DeHerrera <tim@nrdxp.dev>
2022-05-28 21:51:35 -0600

    triton: setup Nvidia card

--- hosts/triton/default.nix
+++ hosts/triton/default.nix
@@ -30,4 +30,30 @@
   environment.etc."tmpfiles.d/home.conf".text = ''
     Q /home 0755 - - -
   '';
+
+  # nvidia
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+    prime = {
+      offload.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "nvrun" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec -a "$0" "$@"
+    '')
+  ];
 }
