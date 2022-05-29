a82aad36ec0b6bbecff0e5791d1045d55655d411
Timothy DeHerrera <tim@nrdxp.dev>
2022-05-28 20:57:39 -0600

    nrd: add to keys group

--- profiles/users/nrd.nix
+++ profiles/users/nrd.nix
@@ -2,9 +2,11 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
 
+  users.mutableUsers = false;
+
   users.users.nrd = {
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" "libvirtd" "media"];
+    extraGroups = ["wheel" "networkmanager" "libvirtd" "media" "keys"];
     hashedPassword = "$6$wvQOaBh8ZDb6sChU$JEGVARG31.mwbIwzzsLyFGTaKBmtE5Xlgq2UE3HhKjT2C6Bf5vy/mSgvFf52iGP0aNWUIA31JzcihEAImlM5I1";
   };
 
