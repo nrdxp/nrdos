85c1e976893a2d024d7ce4171c1bf07d5f27c79a
Timothy DeHerrera <tim@nrdxp.dev>
2022-05-30 16:28:37 -0600

    helix: switch to upstream

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
   inputs.vivarium.url = "github:nrdxp/vivarium";
   inputs.nix.url = "github:nixos/nix/2.8-maintenance";
-  inputs.helix.url = "github:nrdxp/helix/update-nix";
+  inputs.helix.url = "github:helix-editor/helix";
   inputs.registry.url = "github:NixOS/flake-registry";
   inputs.registry.flake = false;
 
