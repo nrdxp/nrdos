a21343e3479116dd3a88040a50a0e919212e8222
Timothy DeHerrera <tim@nrdxp.dev>
2022-06-01 18:03:06 -0600

    im(hack): manual launchers for electron apps

--- profiles/graphical/im/default.nix
+++ profiles/graphical/im/default.nix
@@ -5,4 +5,15 @@
     signal-desktop
     slack
   ];
+  environment.shellAliases = let
+    launcherHack = cmd: ''
+      ( rundir=/run/user/$UID/${cmd}
+        mkdir -p $rundir ''${XDG_CACHE_DIR:=~/.cache}/${cmd} && cd $rundir
+        ${cmd} &> $XDG_CACHE_DIR/${cmd}/log &!
+      )&!
+    '';
+  in {
+    sl = launcherHack "slack";
+    ma = launcherHack "element-desktop";
+  };
 }
