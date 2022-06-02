f074683fd0b76d8194e4929c983b39b193eb4e66
Timothy DeHerrera <tim@nrdxp.dev>
2022-06-01 18:01:49 -0600

    triton: add profile sync daemon

--- hosts/triton/default.nix
+++ hosts/triton/default.nix
@@ -16,6 +16,22 @@
 
   services.pcscd.enable = true;
 
+  # profile-sync daemon
+  services.psd.enable = true;
+  systemd.user.services.psd.path = [ pkgs.glib ];
+  security.sudo.execWheelOnly = true;
+  security.sudo.extraRules = [
+    {
+      users = ["nrd"];
+      commands = [
+        {
+          command = "${pkgs.profile-sync-daemon}/bin/psd-overlay-helper";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
   security.pam.yubico = {
     enable = true;
     mode = "challenge-response";
