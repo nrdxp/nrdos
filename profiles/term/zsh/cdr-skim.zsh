9775ebd0a836fe7f751d721252ba7137725d5b0f
Timothy DeHerrera <tim@nrdxp.dev>
2022-06-08 13:31:21 -0700

    zsh: augment alt+c command to search any git repo

--- profiles/term/zsh/cdr-skim.zsh
+++ profiles/term/zsh/cdr-skim.zsh
@@ -1,1 +1,5 @@
-while read line; do line=${(Q)line}; [[ -d $line ]] && echo $line; done < $HOME/.cache/zsh-cdr/recent-dirs
+declare -a dirs
+dirs+=( $(while read line; do line=${(Q)line}; [[ -d $line ]] && echo $line; done < $HOME/.cache/zsh-cdr/recent-dirs) )
+dirs+=( $(fd --exact-depth=3 -t d . $HOME/git) )
+
+printf "%s\n" ${(u)dirs[@]%/}
