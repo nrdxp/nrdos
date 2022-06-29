9281d41f8401cc02575fcd2ca321fd17325fa351
Timothy DeHerrera <tim@nrdxp.dev>
2022-06-29 13:48:59 -0700

    kitty: configure kitty

--- profiles/graphical/hm-kitty/default.nix
+++ profiles/graphical/hm-kitty/default.nix
@@ -1,6 +1,27 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.file.".zshrc".text = ''
+    if test -n "$KITTY_INSTALLATION_DIR"; then
+        export KITTY_SHELL_INTEGRATION="enabled"
+        autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+        kitty-integration
+        unfunction kitty-integration
+    fi
+  '';
   programs.kitty = {
     enable = true;
     theme = "Snazzy";
+    font = {
+      package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+      name = "Fira Code Regular Nerd Font Complete Mono";
+      size = 9;
+    };
+    settings = {
+      disable_ligatures = "cursor";
+      shell_integration = "disabled";
+    };
   };
 }
